35468fead5a3381449888dae758a2fa48c0f6a01
yonadav-labs <yonadav.barilan.upwork@gmail.com>
2019-01-25 10:15:36 -0800

    add columns to GR_PERSONEN, revise with field names

--- matcher.py
+++ matcher.py
@@ -8,7 +8,6 @@ import pdb
 DEBUG = True
 
 T1 = 90
-T2 = 85
 
 golden_records_key = []
 golden_records_val = []
@@ -17,20 +16,21 @@ def build_record(lst):
     return ' '.join([str(jj).lower().replace('.', '').replace(',', ' ') 
                     for jj in lst if jj])
 
+
 def process_record(cursor, ii, key, grade, match):
     # insert into corresponding tables
     sql = '''INSERT INTO [dbo].[STG_GR_Adressen]([Straat],[Postcode],[Gemeente],[PersonenKey]) 
              values (?, ?, ?, ?)'''
-    cursor.execute(sql, (ii[13], ii[14], ii[15], key))
+    cursor.execute(sql, (ii.STRAAT, ii.POSTKODE, ii.GemeenteOrig, key))
 
     sql = '''INSERT INTO [dbo].[STG_GR_Contacten]([Telefoon],[Email],[Gemeente],[GSM],[Taalcode],[PersonenKey]) 
              values (?, ?, ?, ?, ?, ?)'''
-    cursor.execute(sql, (ii[17], ii[28], ii[15], ii[29], ii[22], key))
+    cursor.execute(sql, (ii.TELEFOON, ii.EMAIL, ii.GemeenteOrig, ii.GSM, ii.TAALKODE, key))
 
     sql = '''INSERT INTO [dbo].[STG_GR_Link]([PECLEUNIK],[PersonenKey],[Percent]) values (?, ?, ?)'''
-    cursor.execute(sql, (ii[0], key, grade))
+    cursor.execute(sql, (ii.PECLEUNIK, key, grade))
     # set processed = true
-    sql = "update [Staging].[dbo].[STG_Personen] set processed=1 where PECLEUNIK={};".format(ii[0])
+    sql = "update [Staging].[dbo].[STG_Personen] set processed=1 where PECLEUNIK={};".format(ii.PECLEUNIK)
     cursor.execute(sql)
     cursor.commit()
 
@@ -40,8 +40,8 @@ def process_record(cursor, ii, key, grade, match):
 
 
 def compare(ii):
-    birthday = str(ii[11].date()) if ii[11] else ''
-    item = [ii[5], ii[6], ii[8], ii[9], birthday]
+    birthday = str(ii.GEBDATUM.date()) if ii.GEBDATUM else ''
+    item = [ii.Naam, ii.voornaam, ii.RRNUMMER, ii.BTWNR, birthday]
     entity = build_record(item)
 
     if not entity:
@@ -49,13 +49,15 @@ def compare(ii):
 
     return process.extractOne(entity, golden_records_val, scorer=fuzz.token_set_ratio)
 
+
 def main(cursor):
     global golden_records_key
     global golden_records_val
 
+    pdb.set_trace()
     # DUMP WEBHISTO
-    cursor.execute('''INSERT INTO [Omerus].[dbo].[STG_GR_Personen]([Naam],[Voornaam],[RRNummer],[GebDatum], [PECLEUNIK])
-                      SELECT Min(FullName), Min(FirstName), RRNUMMER, Min(DateOfBirth) , Min(PECLEUNIK)
+    cursor.execute('''INSERT INTO [Omerus].[dbo].[STG_GR_Personen]([Naam], [Voornaam], [Achternaam], [FirstNameAll], [RRNummer], [GebDatum])
+                      SELECT Min(FullName), Min(FirstName), Min(LastName), Min(FirstNameAll), RRNUMMER, Min(DateOfBirth)
                       FROM [Staging].[dbo].[STG_RRWebHisto_Personen] 
                       WHERE processed is null and RRNUMMER is not null and RRNUMMER != ''
                       GROUP BY RRNUMMER;''')
@@ -73,8 +75,8 @@ def main(cursor):
     cursor.execute("update [Staging].[dbo].[STG_RRWebHisto_Personen] set processed=1 where RRNUMMER in (select RRNummer from [Omerus].[dbo].[STG_GR_Personen]) and RRNUMMER is not null and RRNUMMER != '';")
     cursor.commit()
 
-    cursor.execute('''INSERT INTO [Omerus].[dbo].[STG_GR_Personen]([Naam],[Voornaam],[RRNummer],[GebDatum], [PECLEUNIK])
-                      SELECT Min(Naam), Min(voornaam), RRNUMMER, Min(GEBDATUM) , Min(PECLEUNIK)
+    cursor.execute('''INSERT INTO [Omerus].[dbo].[STG_GR_Personen]([Naam], [Voornaam], [Achternaam], [FirstNameAll], [RRNummer], [GebDatum])
+                      SELECT Min(Naam), Min(voornaam), Min(Achternaam), null, RRNUMMER, Min(GEBDATUM)
                       FROM [Staging].[dbo].[STG_Personen] 
                       WHERE processed is null and Grade in ('A', 'B') and RRNUMMER is not null and RRNUMMER != ''
                       GROUP BY RRNUMMER;''')
@@ -113,7 +115,7 @@ def main(cursor):
     sql = "SELECT [PersonenKey], [Naam], [Voornaam], [BTWNR], [RRNummer], [GebDatum] FROM [dbo].[STG_GR_Personen];"
     res = cursor.execute(sql).fetchall()
 
-    golden_records = { int(ii[0]): build_record(ii[1:]) for ii in res }
+    golden_records = { int(ii.PersonenKey): build_record(ii[1:]) for ii in res }
 
     if not golden_records:
         return
@@ -121,7 +123,6 @@ def main(cursor):
     golden_records_key = list(golden_records.keys())
     golden_records_val = list(golden_records.values())
 
-    # pdb.set_trace()
     # get records with processed = false
     while True:
         sql = "SELECT top 100 * FROM [Staging].[dbo].[STG_Personen] WHERE processed is NULL and grade in ('A', 'B');"
@@ -137,9 +138,9 @@ def main(cursor):
                 process_record(cursor, ii, key, grade, match)
             elif grade > 0:
                 # insert into STG_GR_Personen
-                sql = '''INSERT INTO [dbo].[STG_GR_Personen]([Naam],[Voornaam], [RRNummer], [BTWnr],[Geslacht],[GebDatum],[GebPlaats],[PECLEUNIK])
-                         values(?,?,?,?,?,?,?,?)'''
-                cursor.execute(sql, (ii[5], ii[6], str(uuid.uuid4()), ii[9], ii[30], ii[11], ii[23], ii[0]))
+                sql = '''INSERT INTO [dbo].[STG_GR_Personen]([Naam], [Voornaam], [Achternaam], [FirstNameAll], [RRNummer], [BTWnr], [Geslacht], [GebDatum], [GebPlaats])
+                         values(?,?,?,?,?,?,?,?,?,?)'''
+                cursor.execute(sql, (ii.Naam, ii.voornaam, str(uuid.uuid4()), ii.Achternaam, None, ii.BTWNR, ii.GESLACHT, ii.GEBDATUM, ii.GEBPLAATS))
                 cursor.commit()
                 # update golden_records
                 sql = "SELECT top 1 [PersonenKey], [Naam], [Voornaam], [BTWNR], [GebDatum] FROM [dbo].[STG_GR_Personen] order by PersonenKey desc;"
@@ -151,7 +152,6 @@ def main(cursor):
                 golden_records_key = list(golden_records.keys())
                 golden_records_val = list(golden_records.values())
                 process_record(cursor, ii, key, 100, None)
-
 
     # get records with processed = false
     while True:
